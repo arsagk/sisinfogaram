69567207996f816ca19342114a1b36f496fd209c
arsagk <arsagibrankhalfani@gmail.com>
2025-06-21 16:29:03 +0700

    periksa status

--- bootstrap/ssr/assets/useSwal-3ab423ea.js
+++ bootstrap/ssr/assets/useSwal-3ab423ea.js
@@ -1,14 +1,13 @@
 import Swal from "sweetalert2";
-const confirm = ({ title, text, icon }) => Swal.fire({
-  title: title ? title : "Are you sure?",
-  text: text ? text : "You won't be able to revert this!",
-  icon: icon ? icon : "warning",
-  showCancelButton: true,
-  confirmButtonColor: "#3085d6",
-  cancelButtonColor: "#d33",
-  confirmButtonText: "Yes"
-});
+const confirm = ({ title, text, icon }) =>
+    Swal.fire({
+        title: title ? title : "Are you sure?",
+        text: text ? text : "You won't be able to revert this!",
+        icon: icon ? icon : "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes",
+    });
 const useSwal = { confirm };
-export {
-  useSwal as u
-};
+export { useSwal as u };
